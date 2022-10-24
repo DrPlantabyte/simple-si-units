80af558377c38c119cce8e538eb5de15e636e5cb
Chris Hall <explosivegnome@yahoo.com>
2022-10-24 20:07:00 +1100

    added auto-incrementing build number and put some test code in the main lib file

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,94 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+//! # Simple SI Units
+//! Work in progress...
+
+use core::ops::Div;
+
+/// Placeholder: Work in progress
+trait Length {
+	type NumType;
+
+	/// Placeholder: Work in progress
+	fn meters(self) -> Self::NumType; 
 }
 
+/// Placeholder: Work in progress
+trait Time {
+	type NumType;
+
+	/// Placeholder: Work in progress	
+	fn seconds(self) -> Self::NumType; 
+}
+
+/// Placeholder: Work in progress
+trait Velocity {
+	type NumType;
+	
+	/// Placeholder: Work in progress
+	fn meters_per_second(self) -> Self::NumType; 
+}
+
+
+/// Placeholder: Work in progress
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct GLength<MT> {
+	m: MT
+}
+
+impl<MT> Length for GLength<MT> {
+	type NumType = MT;
+	fn meters(self) -> Self::NumType{
+		self.m
+	}
+}
+
+/// Placeholder: Work in progress
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct GTime<MT> {
+	s: MT
+}
+
+impl<MT> Time for GTime<MT> {
+	type NumType = MT;
+	fn seconds(self) -> Self::NumType{
+		self.s
+	}
+}
+
+/// Placeholder: Work in progress
+#[derive(Debug, Copy, Clone, PartialEq)]
+struct GVelocity<MT> {
+	mps: MT
+}
+
+impl<MT> Velocity for GVelocity<MT> {
+	type NumType = MT;
+	fn meters_per_second(self) -> Self::NumType{
+		self.mps
+	}
+}
+
+
+impl<NT, TT> Div<TT> for GLength<NT>
+where TT: Time<NumType=NT>, NT: Div<NT> + std::ops::Div<Output = NT>
+{
+	type Output = GVelocity<NT>;
+	fn div(self, rhs: TT) -> Self::Output {
+		let y: GVelocity<NT> = GVelocity{mps: self.meters() / rhs.seconds()};
+		return y;
+	}
+}
+
+/// Unit tests
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+	/// Placeholder: Work in progress
+	#[test]
+	fn velocity_api() {
+		let d = GLength{m: 5.0f64};
+		let t = GTime{s: 2.0f64};
+		let v = d / t;
+		assert_eq!(v, GVelocity{mps: 2.5f64});
+	}
 }
