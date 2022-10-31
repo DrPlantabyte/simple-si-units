de120c28dd467e1b31b445e671b8840acfc7d456
Chris Hall <explosivegnome@yahoo.com>
2022-11-01 09:33:38 +1100

    README updates and WIP on API design (#3)
    * playing with GitHub workflow badges

--- simple-si-units/tests/integration_tests.rs
+++ simple-si-units/tests/integration_tests.rs
@@ -24,6 +24,71 @@ fn weighted_sum<T: UnitData>(a: HyperVelocity<T>, b: HyperVelocity<T>, weight: f
 	return weight*a + (1.-weight)*b;
 }
 
+#[derive(Unit, Debug, Copy, Clone)]
+struct Mass<T: UnitData>{
+	pub kg: T
+}
+
+#[derive(Unit, Debug, Copy, Clone)]
+struct Distance<T: UnitData>{
+	pub meters: T
+}
+
+#[derive(Unit, Debug, Copy, Clone)]
+struct Time<T: UnitData>{
+	pub seconds: T
+}
+
+#[derive(Unit, Debug, Copy, Clone)]
+struct Velocity<T: UnitData>{
+	pub mps: T
+}
+
+#[derive(Unit, Debug, Copy, Clone)]
+struct Acceleration<T: UnitData>{
+	pub mps2: T
+}
+
+impl<T> std::ops::Div<Time<T>> for Distance<T> where T: UnitData {
+	type Output = Velocity<T>;
+
+	fn div(self, rhs: Time<T>) -> Self::Output {
+		Velocity{mps: self.meters / rhs.seconds}
+	}
+}
+
+impl<T> std::ops::Div<Time<T>> for Velocity<T> where T: UnitData {
+	type Output = Acceleration<T>;
+
+	fn div(self, rhs: Time<T>) -> Self::Output {
+		Acceleration{mps2: self.mps / rhs.seconds}
+	}
+}
+
+#[derive(Debug, Clone)]
+struct MassPoint {
+	mass: Mass<f64>,
+	pos: [Distance<f64>; 3],
+	vel: [Velocity<f64>; 3],
+	accel: [Acceleration<f64>; 3],
+}
+
+const G: f64 = 6.67408e-1; // m3 kg-1 s-2
+
+struct LCGRand {
+	seed: u64
+}
+impl LCGRand {
+	fn rand_f64(mut self) -> f64 {
+		self.seed = (self.seed * 0x5DEECE66Du64 + 0xBu64) & 0xFFFFFFFFFFFFu64;
+		return ((self.state & 0xFFFFFFFFu64) as f64 / 0xFFFFFFFFu64 as f64) * 2.0 - 1.;
+	}
+}
+
+fn populate_system() -> Vec<MassPoint> {
+	let prng =
+}
+
 /*
 // if only this worked:
 #[derive(Debug)]
