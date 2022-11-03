c6b2e92c8b036680dcf68fb4106c2163e007ebb0
CCH <explosivegnome@yahoo.com>
2022-11-04 09:35:30 +1100

    working on API design

--- simple-si-units/tests/integration_tests.rs
+++ simple-si-units/tests/integration_tests.rs
@@ -1,5 +1,16 @@
+use std::any::Any;
 use simple_si_units::{Unit, UnitData};
 
+/*
+How it will work:
+unit type structs will template <T: number-like>, but not impose and from<> restrictions
+but the implemented methods which require multiplying by a coefficient (eg 1000*m for km)
+will add +From<f64> to the where clause
+
+Users who want f32 or int implementations will still be able to use the structs, but
+will need to use the num crate or implement their own wrapper type that implements
+From<f64>
+*/
 
 #[derive(Unit, Debug, Copy, Clone)]
 struct Bananas<DT>{
@@ -28,10 +39,10 @@ fn weighted_sum<T: UnitData>(a: HyperVelocity<T>, b: HyperVelocity<T>, weight: f
 struct Mass<T: UnitData>{
 	pub kg: T
 }
-impl<T> Mass<T> where T: UnitData {
-	const EARTH_MASS_KG: T = T::from(5.972e24);
+impl<T> Mass<T> where T: UnitData+From<f64> {
 	fn from_earth_mass(earth_mass: T) -> Self {
-		Mass{kg: Self::EARTH_MASS_KG * earth_mass}
+		let EARTH_MASS_KG: T = T::from(5.972e24f64);
+		Mass{kg: EARTH_MASS_KG * earth_mass}
 	}
 }
 
@@ -40,12 +51,18 @@ struct Distance<T: UnitData>{
 	pub meters: T
 }
 
-impl<T> Distance<T> where T: UnitData {
-	const AU_M: T = T::from(1.495979e11);
+impl<T> Distance<T> where T: UnitData+From<f64> {
 	fn from_au(au: T) -> Self{
-		Distance{meters: Self::AU_M * au}
+		let AU_M = T::from(1.495979e11f64);
+		Distance{meters: AU_M * au}
 	}
 }
+// impl Distance<f32>  {
+// 	const AU_M_f32: f32 = 1.495979e11 as f32;
+// 	fn from_au(au: f32) -> Self{
+// 		Distance{meters: Self::AU_M_f32 * au}
+// 	}
+// }
 
 #[derive(Unit, Debug, Copy, Clone)]
 struct Time<T: UnitData>{
@@ -92,14 +109,24 @@ struct LCGRand {
 	seed: u64
 }
 impl LCGRand {
-	fn rand_f64(mut self) -> f64 {
+	fn rand_f64(&mut self) -> f64 {
 		self.seed = (self.seed * 0x5DEECE66Du64 + 0xBu64) & 0xFFFFFFFFFFFFu64;
 		return (self.seed & 0xFFFFFFFFu64) as f64 / 0xFFFFFFFFu64 as f64;
 	}
 }
 
 fn populate_system() -> Vec<MassPoint> {
-	let prng = LCGRand{seed: 1234876};
+	let mut prng = LCGRand{seed: 1234876};
+	//
+	let _unused = Distance{meters: 11.11f32};
+	use num_bigfloat::BigFloat;
+	let _unused2 = Distance{meters: BigFloat::from(123.456)};
+	use num_complex::Complex;
+	let _unused3 = Distance{meters: Complex::from(123.456)};
+	// let _unused4 = Mass::from_earth_mass((prng.rand_f64() * 10.) as f32);
+	let _unused4 = Mass::from_earth_mass((prng.rand_f64() * 10.) as Float32);
+	println!("{:?}", _unused4.type_id());
+	//
 	let mut system: Vec<MassPoint> = Vec::new();
 	for _ in 0..12 {
 		system.push(MassPoint{
