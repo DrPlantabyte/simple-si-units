12c1ecd1781bde6badf047b9ed62f1de0a725c93
CCH <explosivegnome@yahoo.com>
2023-02-21 10:23:59 +1100

    added hashing, equality, and comparisons to proc macro

--- simple-si-units-macros/src/lib.rs
+++ simple-si-units-macros/src/lib.rs
@@ -4,8 +4,6 @@ use syn::*;
 
 // Test with $ rm ./tests/expand/derive_tester.expanded.rs ; cargo +nightly test -- --nocapture
 
-// TODO: <, <=, >, >=, ==
-
 #[proc_macro_derive(UnitStruct)]
 pub fn derive_unit(tokens: TokenStream) -> TokenStream {
 	// convert the input tokens into an ast, specially from a derive
@@ -41,19 +39,65 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 	let data_name = &fields[0].ident.as_ref().unwrap();
 	let data_type = &fields[0].ty;
 	let gen = quote! {
+		#[doc="This struct implements the Copy marker trait if it's member data type also has the \
+		Copy trait"]
 		impl<#data_type> std::marker::Copy for #name<#data_type>
 			where #data_type: simple_si_units_core::NumLike + std::marker::Copy { }
+		#[doc="This struct implements the PartialEq trait if it's member data type also has the \
+		PartialEq trait"]
+		impl<#data_type> std::cmp::PartialEq for #name<#data_type>
+			where #data_type: simple_si_units_core::NumLike + std::cmp::PartialEq {
+			fn eq(&self, other: &Self) -> bool {
+				#data_type::eq(&self.#data_name, &other.#data_name)
+			}
+			fn ne(&self, other: &Self) -> bool {
+				#data_type::ne(&self.#data_name, &other.#data_name)
+			}
+		}
+		#[doc="This struct implements the std::cmp::Eq trait if it's member data type also has the \
+		std::cmp::Eq trait"]
+		impl<#data_type> std::cmp::Eq for #name<#data_type>
+			where #data_type: simple_si_units_core::NumLike + std::cmp::PartialEq + std::cmp::Eq {}
+		#[doc="This struct implements the Hash trait if it's member data type also has the \
+		Hash trait"]
+		impl<#data_type> std::hash::Hash for #name<#data_type>
+			where #data_type: simple_si_units_core::NumLike + std::hash::Hash {
+			fn hash<H: std::hash::Hasher>(&self, hasher: &mut H) {
+				#data_type::hash(&self.#data_name, hasher);
+			}
+		}
+		#[doc="This struct implements the PartialOrd trait if it's member data type also has the \
+		PartialOrd trait"]
+		impl<#data_type> std::cmp::PartialOrd for #name<#data_type>
+			where #data_type: simple_si_units_core::NumLike + std::cmp::PartialOrd {
+			fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+				#data_type::partial_cmp(&self.#data_name, &other.#data_name)
+			}
+		}
+		#[doc="This struct implements the Ord trait if it's member data type also has the \
+		Ord trait"]
+		impl<#data_type> std::cmp::Ord for #name<#data_type>
+			where #data_type: simple_si_units_core::NumLike + std::cmp::Ord {
+			fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+				#data_type::cmp(&self.#data_name, &other.#data_name)
+			}
+		}
+
+		#[doc="Adding two unit values of the same type returns a new unit value of the same type"]
 		impl<#data_type: simple_si_units_core::NumLike> std::ops::Add<Self> for #name<#data_type> {
 			type Output = Self;
 			fn add(self, rhs: Self) -> Self::Output {
 				return Self{#data_name: self.#data_name + rhs.#data_name}
 			}
 		}
+		#[doc="Adds the given unit value to this unit value"]
 		impl<#data_type: simple_si_units_core::NumLike> std::ops::AddAssign for #name<#data_type> {
 			fn add_assign(&mut self, rhs: Self){
 				self.#data_name += rhs.#data_name;
 			}
 		}
+		#[doc="Subtracting two unit values of the same type returns a new unit value of the same \
+		type"]
 		impl<#data_type: simple_si_units_core::NumLike> std::ops::Sub<Self> for
 		#name<#data_type> {
 			type Output = Self;
@@ -61,11 +105,13 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return Self{#data_name: self.#data_name - rhs.#data_name}
 			}
 		}
+		#[doc="Subtracts the given unit value from this unit value"]
 		impl<#data_type: simple_si_units_core::NumLike> std::ops::SubAssign for #name<#data_type> {
 			fn sub_assign(&mut self, rhs: Self){
 				self.#data_name -= rhs.#data_name;
 			}
 		}
+		#[doc="Dividing a unit value by another of the same type returns a scalar value"]
 		impl<#data_type: simple_si_units_core::NumLike> std::ops::Div<Self> for
 		#name<#data_type> {
 			type Output = #data_type;
@@ -73,6 +119,7 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return self.#data_name / rhs.#data_name;
 			}
 		}
+		#[doc="Dividing a unit value by a scalar value returns a unit value"]
 		impl<#data_type: simple_si_units_core::NumLike> std::ops::Div<#data_type> for
 		#name<#data_type> {
 			type Output = Self;
@@ -80,11 +127,13 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return Self{#data_name: self.#data_name / rhs}
 			}
 		}
+		#[doc="Divides this unit value by a scalar"]
 		impl<#data_type: simple_si_units_core::NumLike> std::ops::DivAssign<#data_type> for #name<#data_type> {
 			fn div_assign(&mut self, rhs: #data_type){
 				self.#data_name /= rhs;
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value"]
 		impl<#data_type: simple_si_units_core::NumLike> std::ops::Mul<#data_type> for
 		#name<#data_type> {
 			type Output = Self;
@@ -92,11 +141,13 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return Self{#data_name: self.#data_name * rhs}
 			}
 		}
+		#[doc="Multiplies this unit value by a scalar"]
 		impl<#data_type: simple_si_units_core::NumLike> std::ops::MulAssign<#data_type> for #name<#data_type> {
 			fn mul_assign(&mut self, rhs: #data_type){
 				self.#data_name *= rhs;
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value"]
 		impl<#data_type>
 		std::ops::Mul<#name<#data_type>> for
 		 f64 where #data_type: simple_si_units_core::NumLike + From<f64>{
@@ -105,6 +156,7 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return #name{#data_name: #data_type::from(self) * rhs.#data_name}
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value"]
 		impl<#data_type>
 		std::ops::Mul<#name<#data_type>> for
 		 f32 where #data_type: simple_si_units_core::NumLike + From<f32>{
@@ -113,6 +165,7 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return #name{#data_name: #data_type::from(self) * rhs.#data_name}
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value"]
 		impl<#data_type>
 		std::ops::Mul<#name<#data_type>> for
 		 u8 where #data_type: simple_si_units_core::NumLike + From<u8>{
@@ -121,6 +174,7 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return #name{#data_name: #data_type::from(self) * rhs.#data_name}
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value"]
 		impl<#data_type>
 		std::ops::Mul<#name<#data_type>> for
 		 i8 where #data_type: simple_si_units_core::NumLike + From<i8>{
@@ -129,6 +183,7 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return #name{#data_name: #data_type::from(self) * rhs.#data_name}
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value"]
 		impl<#data_type>
 		std::ops::Mul<#name<#data_type>> for
 		 u16 where #data_type: simple_si_units_core::NumLike + From<u16>{
@@ -137,6 +192,7 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return #name{#data_name: #data_type::from(self) * rhs.#data_name}
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value"]
 		impl<#data_type>
 		std::ops::Mul<#name<#data_type>> for
 		 i16 where #data_type: simple_si_units_core::NumLike + From<i16>{
@@ -145,6 +201,7 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return #name{#data_name: #data_type::from(self) * rhs.#data_name}
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value"]
 		impl<#data_type>
 		std::ops::Mul<#name<#data_type>> for
 		 u32 where #data_type: simple_si_units_core::NumLike + From<u32>{
@@ -153,6 +210,7 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return #name{#data_name: #data_type::from(self) * rhs.#data_name}
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value"]
 		impl<#data_type>
 		std::ops::Mul<#name<#data_type>> for
 		 i32 where #data_type: simple_si_units_core::NumLike + From<i32>{
@@ -161,6 +219,7 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return #name{#data_name: #data_type::from(self) * rhs.#data_name}
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value"]
 		impl<#data_type>
 		std::ops::Mul<#name<#data_type>> for
 		 u64 where #data_type: simple_si_units_core::NumLike + From<u64>{
@@ -169,6 +228,7 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return #name{#data_name: #data_type::from(self) * rhs.#data_name}
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value"]
 		impl<#data_type>
 		std::ops::Mul<#name<#data_type>> for
 		 i64 where #data_type: simple_si_units_core::NumLike + From<i64>{
@@ -177,6 +237,7 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return #name{#data_name: #data_type::from(self) * rhs.#data_name}
 			}
 		}
+		#[doc="Flips the sign of this unit value"]
 		impl<#data_type: simple_si_units_core::NumLike> std::ops::Neg for
 		#name<#data_type> {
 			type Output = Self;
@@ -186,12 +247,16 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 		}
 
 		// ref operators automatically clone the referenced data for convenient ergonomics
+		#[doc="Adding two unit values of the same type returns a new unit value of the same type \
+		(automatically clones the referenced data for convenient ergonomics)"]
 		impl<#data_type: simple_si_units_core::NumLike> std::ops::Add<Self> for &#name<#data_type> {
 			type Output = #name<#data_type>;
 			fn add(self, rhs: Self) -> Self::Output {
 				return Self::Output{#data_name: self.#data_name.clone() + rhs.#data_name.clone()}
 			}
 		}
+		#[doc="Subtracting two unit values of the same type returns a new unit value of the same \
+		type (automatically clones the referenced data for convenient ergonomics)"]
 		impl<#data_type: simple_si_units_core::NumLike> std::ops::Sub<Self> for
 		&#name<#data_type> {
 			type Output = #name<#data_type>;
@@ -199,6 +264,8 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return Self::Output{#data_name: self.#data_name.clone() - rhs.#data_name.clone()}
 			}
 		}
+		#[doc="Dividing a unit value by another of the same type returns a scalar value \
+		(automatically clones the referenced data for convenient ergonomics)"]
 		impl<#data_type: simple_si_units_core::NumLike> std::ops::Div<Self> for
 		&#name<#data_type> {
 			type Output = #data_type;
@@ -206,6 +273,8 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return self.#data_name.clone() / rhs.#data_name.clone();
 			}
 		}
+		#[doc="Dividing a unit value by a scalar value returns a unit value (automatically clones \
+		the referenced data for convenient ergonomics)"]
 		impl<#data_type: simple_si_units_core::NumLike> std::ops::Div<#data_type> for
 		&#name<#data_type> {
 			type Output = #name<#data_type>;
@@ -213,6 +282,8 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return Self::Output{#data_name: self.#data_name.clone() / rhs}
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value (automatically \
+		clones the referenced data for convenient ergonomics)"]
 		impl<#data_type: simple_si_units_core::NumLike> std::ops::Mul<#data_type> for
 		&#name<#data_type> {
 			type Output = #name<#data_type>;
@@ -220,6 +291,8 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return Self::Output{#data_name: self.#data_name.clone() * rhs}
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value (automatically \
+		clones the referenced data for convenient ergonomics)"]
 		impl<#data_type>
 		std::ops::Mul<&#name<#data_type>> for
 		 f64 where #data_type: simple_si_units_core::NumLike + From<f64>{
@@ -228,6 +301,8 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return #name{#data_name: #data_type::from(self) * rhs.#data_name.clone()}
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value (automatically \
+		clones the referenced data for convenient ergonomics)"]
 		impl<#data_type>
 		std::ops::Mul<&#name<#data_type>> for
 		 f32 where #data_type: simple_si_units_core::NumLike + From<f32>{
@@ -236,6 +311,8 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return #name{#data_name: #data_type::from(self) * rhs.#data_name.clone()}
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value (automatically \
+		clones the referenced data for convenient ergonomics)"]
 		impl<#data_type>
 		std::ops::Mul<&#name<#data_type>> for
 		 u8 where #data_type: simple_si_units_core::NumLike + From<u8>{
@@ -244,6 +321,8 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return #name{#data_name: #data_type::from(self) * rhs.#data_name.clone()}
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value (automatically \
+		clones the referenced data for convenient ergonomics)"]
 		impl<#data_type>
 		std::ops::Mul<&#name<#data_type>> for
 		 i8 where #data_type: simple_si_units_core::NumLike + From<i8>{
@@ -252,6 +331,8 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return #name{#data_name: #data_type::from(self) * rhs.#data_name.clone()}
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value (automatically \
+		clones the referenced data for convenient ergonomics)"]
 		impl<#data_type>
 		std::ops::Mul<&#name<#data_type>> for
 		 u16 where #data_type: simple_si_units_core::NumLike + From<u16>{
@@ -260,6 +341,8 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return #name{#data_name: #data_type::from(self) * rhs.#data_name.clone()}
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value (automatically \
+		clones the referenced data for convenient ergonomics)"]
 		impl<#data_type>
 		std::ops::Mul<&#name<#data_type>> for
 		 i16 where #data_type: simple_si_units_core::NumLike + From<i16>{
@@ -268,6 +351,8 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return #name{#data_name: #data_type::from(self) * rhs.#data_name.clone()}
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value (automatically \
+		clones the referenced data for convenient ergonomics)"]
 		impl<#data_type>
 		std::ops::Mul<&#name<#data_type>> for
 		 u32 where #data_type: simple_si_units_core::NumLike + From<u32>{
@@ -276,6 +361,8 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return #name{#data_name: #data_type::from(self) * rhs.#data_name.clone()}
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value (automatically \
+		clones the referenced data for convenient ergonomics)"]
 		impl<#data_type>
 		std::ops::Mul<&#name<#data_type>> for
 		 i32 where #data_type: simple_si_units_core::NumLike + From<i32>{
@@ -284,6 +371,8 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return #name{#data_name: #data_type::from(self) * rhs.#data_name.clone()}
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value (automatically \
+		clones the referenced data for convenient ergonomics)"]
 		impl<#data_type>
 		std::ops::Mul<&#name<#data_type>> for
 		 u64 where #data_type: simple_si_units_core::NumLike + From<u64>{
@@ -292,6 +381,8 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return #name{#data_name: #data_type::from(self) * rhs.#data_name.clone()}
 			}
 		}
+		#[doc="Multiplying a unit value by a scalar value returns a unit value (automatically \
+		clones the referenced data for convenient ergonomics)"]
 		impl<#data_type>
 		std::ops::Mul<&#name<#data_type>> for
 		 i64 where #data_type: simple_si_units_core::NumLike + From<i64>{
@@ -300,6 +391,8 @@ fn impl_derive_unit(input: &syn::DeriveInput) -> TokenStream {
 				return #name{#data_name: #data_type::from(self) * rhs.#data_name.clone()}
 			}
 		}
+		#[doc="Flips the sign of this unit value (automatically clones the referenced data for \
+		convenient ergonomics)"]
 		impl<#data_type: simple_si_units_core::NumLike> std::ops::Neg for
 		&#name<#data_type> {
 			type Output = #name<#data_type>;
