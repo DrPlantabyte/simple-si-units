3387559ae1e8f9909fc1e9d17f7fb48735499da9
Chris Hall <explosivegnome@yahoo.com>
2023-04-20 19:40:14 +0800

    added unit tests for uom integration

--- simple-si-units/tests/uom_integration_tests.rs
+++ simple-si-units/tests/uom_integration_tests.rs
@@ -113,10 +113,296 @@ mod uom_integration {
 
 	#[test]
 	fn into_uom_test(){
-		todo!()
+		let x: f64 = 2.5;
+		assert!(uom::si::f64::AmountOfSubstance::new::<uom::si::amount_of_substance::mole>(x)
+			== simple_si_units::base::Amount{mol: x}.into());
+		// assert!(uom::si::f64::ElectricCurrent::new::<uom::si::electric_current::ampere>(x)
+		// 	== simple_si_units::base::Current{A: x}.into());
+		// assert!(uom::si::f64::Length::new::<uom::si::length::meter>(x)
+		// 	== simple_si_units::base::Distance{m: x}.into());
+		// assert!(uom::si::f64::LuminousIntensity::new::<uom::si::luminous_intensity::candela>(x)
+		// 	== simple_si_units::base::Luminosity{cd: x}.into());
+		// assert!(uom::si::f64::Mass::new::<uom::si::mass::kilogram>(x)
+		// 	== simple_si_units::base::Mass{kg: x}.into());
+		// assert!(uom::si::f64::ThermodynamicTemperature::new::<uom::si::thermodynamic_temperature::kelvin>(x)
+		// 	== simple_si_units::base::Temperature{K: x}.into());
+		// assert!(uom::si::f64::Time::new::<uom::si::time::second>(x)
+		// 	== simple_si_units::base::Time{s: x}.into());
+		// assert!(uom::si::f64::CatalyticActivity::new::<uom::si::catalytic_activity::mole_per_second>(x)
+		// 	== simple_si_units::chemical::CatalyticActivity{molps: x}.into());
+		// assert!(uom::si::f64::MolarConcentration::new::<uom::si::molar_concentration::mole_per_cubic_meter>(x)
+		// 	== simple_si_units::chemical::Concentration{molpm3: x}.into());
+		// assert!(uom::si::f64::Capacitance::new::<uom::si::capacitance::farad>(x)
+		// 	== simple_si_units::electromagnetic::Capacitance{F: x}.into());
+		// assert!(uom::si::f64::ElectricCharge::new::<uom::si::electric_charge::coulomb>(x)
+		// 	== simple_si_units::electromagnetic::Charge{C: x}.into());
+		// assert!(uom::si::f64::Luminance::new::<uom::si::luminance::candela_per_square_meter>(x)
+		// 	== simple_si_units::electromagnetic::Illuminance{lux: x}.into());
+		// assert!(uom::si::f64::Inductance::new::<uom::si::inductance::henry>(x)
+		// 	== simple_si_units::electromagnetic::Inductance{H: x}.into());
+		// assert!(uom::si::f64::MagneticFlux::new::<uom::si::magnetic_flux::weber>(x)
+		// 	== simple_si_units::electromagnetic::MagneticFlux{Wb: x}.into());
+		// assert!(uom::si::f64::MagneticFluxDensity::new::<uom::si::magnetic_flux_density::tesla>(x)
+		// 	== simple_si_units::electromagnetic::MagneticFluxDensity{T: x}.into());
+		// assert!(uom::si::f64::ElectricalResistance::new::<uom::si::electrical_resistance::ohm>(x)
+		// 	== simple_si_units::electromagnetic::Resistance{Ohm: x}.into());
+		// assert!(uom::si::f64::ElectricPotential::new::<uom::si::electric_potential::volt>(x)
+		// 	== simple_si_units::electromagnetic::Voltage{V: x}.into());
+		// assert!(uom::si::f64::Angle::new::<uom::si::angle::radian>(x)
+		// 	== simple_si_units::geometry::Angle{rad: x}.into());
+		// assert!(uom::si::f64::Area::new::<uom::si::area::square_meter>(x)
+		// 	== simple_si_units::geometry::Area{m2: x}.into());
+		// assert!(uom::si::f64::Volume::new::<uom::si::volume::cubic_meter>(x)
+		// 	== simple_si_units::geometry::Volume{m3: x}.into());
+		// assert!(uom::si::f64::Acceleration::new::<uom::si::acceleration::meter_per_second_squared>(x)
+		// 	== simple_si_units::mechanical::Acceleration{mps2: x}.into());
+		// assert!(uom::si::f64::AngularAcceleration::new::<uom::si::angular_acceleration::radian_per_second_squared>(x)
+		// 	== simple_si_units::mechanical::AngularAcceleration{radps2: x}.into());
+		// assert!(uom::si::f64::AngularVelocity::new::<uom::si::angular_velocity::radian_per_second>(x)
+		// 	== simple_si_units::mechanical::AngularVelocity{radps: x}.into());
+		// assert!(uom::si::f64::ArealMassDensity::new::<uom::si::areal_mass_density::kilogram_per_square_meter>(x)
+		// 	== simple_si_units::mechanical::AreaDensity{kgpm2: x}.into());
+		// assert!(uom::si::f64::MassDensity::new::<uom::si::mass_density::kilogram_per_cubic_meter>(x)
+		// 	== simple_si_units::mechanical::Density{kgpm3: x}.into());
+		// assert!(uom::si::f64::Energy::new::<uom::si::energy::joule>(x)
+		// 	== simple_si_units::mechanical::Energy{J: x}.into());
+		// assert!(uom::si::f64::Force::new::<uom::si::force::newton>(x)
+		// 	== simple_si_units::mechanical::Force{N: x}.into());
+		// assert!(uom::si::f64::Frequency::new::<uom::si::frequency::hertz>(x)
+		// 	== simple_si_units::mechanical::Frequency{Hz: x}.into());
+		// assert!(uom::si::f64::MomentOfInertia::new::<uom::si::moment_of_inertia::kilogram_square_meter>(x)
+		// 	== simple_si_units::mechanical::MomentOfInertia{kgm2: x}.into());
+		// assert!(uom::si::f64::Momentum::new::<uom::si::momentum::kilogram_meter_per_second>(x)
+		// 	== simple_si_units::mechanical::Momentum{kgmps: x}.into());
+		// assert!(uom::si::f64::Power::new::<uom::si::power::watt>(x)
+		// 	== simple_si_units::mechanical::Power{W: x}.into());
+		// assert!(uom::si::f64::Pressure::new::<uom::si::pressure::pascal>(x)
+		// 	== simple_si_units::mechanical::Pressure{Pa: x}.into());
+		// assert!(uom::si::f64::Torque::new::<uom::si::torque::newton_meter>(x)
+		// 	== simple_si_units::mechanical::Torque{Nm: x}.into());
+		// assert!(uom::si::f64::Velocity::new::<uom::si::velocity::meter_per_second>(x)
+		// 	== simple_si_units::mechanical::Velocity{mps: x}.into());
+		// assert!(uom::si::f64::Radioactivity::new::<uom::si::radioactivity::becquerel>(x)
+		// 	== simple_si_units::nuclear::Radioactivity{Bq: x}.into());
 	}
 	#[test]
-	fn from_uom_test(){
-		todo!()
+	fn from_uom_test_f64(){
+		let x: f64 = 2.5;
+		assert!(simple_si_units::base::Amount::from(
+				uom::si::f64::AmountOfSubstance::new::<uom::si::amount_of_substance::mole>(x)
+			) == simple_si_units::base::Amount{mol: x});
+		assert!(simple_si_units::base::Current::from(
+				uom::si::f64::ElectricCurrent::new::<uom::si::electric_current::ampere>(x)
+			) == simple_si_units::base::Current{A: x});
+		assert!(simple_si_units::base::Distance::from(
+				uom::si::f64::Length::new::<uom::si::length::meter>(x)
+			) == simple_si_units::base::Distance{m: x});
+		assert!(simple_si_units::base::Luminosity::from(
+				uom::si::f64::LuminousIntensity::new::<uom::si::luminous_intensity::candela>(x)
+			) == simple_si_units::base::Luminosity{cd: x});
+		assert!(simple_si_units::base::Mass::from(
+				uom::si::f64::Mass::new::<uom::si::mass::kilogram>(x)
+			) == simple_si_units::base::Mass{kg: x});
+		assert!(simple_si_units::base::Temperature::from(
+				uom::si::f64::ThermodynamicTemperature::new::<uom::si::thermodynamic_temperature::kelvin>(x)
+			) == simple_si_units::base::Temperature{K: x});
+		assert!(simple_si_units::base::Time::from(
+				uom::si::f64::Time::new::<uom::si::time::second>(x)
+			) == simple_si_units::base::Time{s: x});
+		assert!(simple_si_units::chemical::CatalyticActivity::from(
+				uom::si::f64::CatalyticActivity::new::<uom::si::catalytic_activity::mole_per_second>(x)
+			) == simple_si_units::chemical::CatalyticActivity{molps: x});
+		assert!(simple_si_units::chemical::Concentration::from(
+				uom::si::f64::MolarConcentration::new::<uom::si::molar_concentration::mole_per_cubic_meter>(x)
+			) == simple_si_units::chemical::Concentration{molpm3: x});
+		assert!(simple_si_units::electromagnetic::Capacitance::from(
+				uom::si::f64::Capacitance::new::<uom::si::capacitance::farad>(x)
+			) == simple_si_units::electromagnetic::Capacitance{F: x});
+		assert!(simple_si_units::electromagnetic::Charge::from(
+				uom::si::f64::ElectricCharge::new::<uom::si::electric_charge::coulomb>(x)
+			) == simple_si_units::electromagnetic::Charge{C: x});
+		assert!(simple_si_units::electromagnetic::Illuminance::from(
+				uom::si::f64::Luminance::new::<uom::si::luminance::candela_per_square_meter>(x)
+			) == simple_si_units::electromagnetic::Illuminance{lux: x});
+		assert!(simple_si_units::electromagnetic::Inductance::from(
+				uom::si::f64::Inductance::new::<uom::si::inductance::henry>(x)
+			) == simple_si_units::electromagnetic::Inductance{H: x});
+		assert!(simple_si_units::electromagnetic::MagneticFlux::from(
+				uom::si::f64::MagneticFlux::new::<uom::si::magnetic_flux::weber>(x)
+			) == simple_si_units::electromagnetic::MagneticFlux{Wb: x});
+		assert!(simple_si_units::electromagnetic::MagneticFluxDensity::from(
+				uom::si::f64::MagneticFluxDensity::new::<uom::si::magnetic_flux_density::tesla>(x)
+			) == simple_si_units::electromagnetic::MagneticFluxDensity{T: x});
+		assert!(simple_si_units::electromagnetic::Resistance::from(
+				uom::si::f64::ElectricalResistance::new::<uom::si::electrical_resistance::ohm>(x)
+			) == simple_si_units::electromagnetic::Resistance{Ohm: x});
+		assert!(simple_si_units::electromagnetic::Voltage::from(
+				uom::si::f64::ElectricPotential::new::<uom::si::electric_potential::volt>(x)
+			) == simple_si_units::electromagnetic::Voltage{V: x});
+		assert!(simple_si_units::geometry::Angle::from(
+				uom::si::f64::Angle::new::<uom::si::angle::radian>(x)
+			) == simple_si_units::geometry::Angle{rad: x});
+		assert!(simple_si_units::geometry::Area::from(
+				uom::si::f64::Area::new::<uom::si::area::square_meter>(x)
+			) == simple_si_units::geometry::Area{m2: x});
+		assert!(simple_si_units::geometry::Volume::from(
+				uom::si::f64::Volume::new::<uom::si::volume::cubic_meter>(x)
+			) == simple_si_units::geometry::Volume{m3: x});
+		assert!(simple_si_units::mechanical::Acceleration::from(
+				uom::si::f64::Acceleration::new::<uom::si::acceleration::meter_per_second_squared>(x)
+			) == simple_si_units::mechanical::Acceleration{mps2: x});
+		assert!(simple_si_units::mechanical::AngularAcceleration::from(
+				uom::si::f64::AngularAcceleration::new::<uom::si::angular_acceleration::radian_per_second_squared>(x)
+			) == simple_si_units::mechanical::AngularAcceleration{radps2: x});
+		assert!(simple_si_units::mechanical::AngularVelocity::from(
+				uom::si::f64::AngularVelocity::new::<uom::si::angular_velocity::radian_per_second>(x)
+			) == simple_si_units::mechanical::AngularVelocity{radps: x});
+		assert!(simple_si_units::mechanical::AreaDensity::from(
+				uom::si::f64::ArealMassDensity::new::<uom::si::areal_mass_density::kilogram_per_square_meter>(x)
+			) == simple_si_units::mechanical::AreaDensity{kgpm2: x});
+		assert!(simple_si_units::mechanical::Density::from(
+				uom::si::f64::MassDensity::new::<uom::si::mass_density::kilogram_per_cubic_meter>(x)
+			) == simple_si_units::mechanical::Density{kgpm3: x});
+		assert!(simple_si_units::mechanical::Energy::from(
+				uom::si::f64::Energy::new::<uom::si::energy::joule>(x)
+			) == simple_si_units::mechanical::Energy{J: x});
+		assert!(simple_si_units::mechanical::Force::from(
+				uom::si::f64::Force::new::<uom::si::force::newton>(x)
+			) == simple_si_units::mechanical::Force{N: x});
+		assert!(simple_si_units::mechanical::Frequency::from(
+				uom::si::f64::Frequency::new::<uom::si::frequency::hertz>(x)
+			) == simple_si_units::mechanical::Frequency{Hz: x});
+		assert!(simple_si_units::mechanical::MomentOfInertia::from(
+				uom::si::f64::MomentOfInertia::new::<uom::si::moment_of_inertia::kilogram_square_meter>(x)
+			) == simple_si_units::mechanical::MomentOfInertia{kgm2: x});
+		assert!(simple_si_units::mechanical::Momentum::from(
+				uom::si::f64::Momentum::new::<uom::si::momentum::kilogram_meter_per_second>(x)
+			) == simple_si_units::mechanical::Momentum{kgmps: x});
+		assert!(simple_si_units::mechanical::Power::from(
+				uom::si::f64::Power::new::<uom::si::power::watt>(x)
+			) == simple_si_units::mechanical::Power{W: x});
+		assert!(simple_si_units::mechanical::Pressure::from(
+				uom::si::f64::Pressure::new::<uom::si::pressure::pascal>(x)
+			) == simple_si_units::mechanical::Pressure{Pa: x});
+		assert!(simple_si_units::mechanical::Torque::from(
+				uom::si::f64::Torque::new::<uom::si::torque::newton_meter>(x)
+			) == simple_si_units::mechanical::Torque{Nm: x});
+		assert!(simple_si_units::mechanical::Velocity::from(
+				uom::si::f64::Velocity::new::<uom::si::velocity::meter_per_second>(x)
+			) == simple_si_units::mechanical::Velocity{mps: x});
+		assert!(simple_si_units::nuclear::Radioactivity::from(
+				uom::si::f64::Radioactivity::new::<uom::si::radioactivity::becquerel>(x)
+			) == simple_si_units::nuclear::Radioactivity{Bq: x});
 	}
+
+	#[test]
+	fn from_uom_test_f32(){
+		let x: f32 = 2.5;
+		assert!(simple_si_units::base::Amount::from(
+			uom::si::f32::AmountOfSubstance::new::<uom::si::amount_of_substance::mole>(x)
+		) == simple_si_units::base::Amount{mol: x});
+		assert!(simple_si_units::base::Current::from(
+			uom::si::f32::ElectricCurrent::new::<uom::si::electric_current::ampere>(x)
+		) == simple_si_units::base::Current{A: x});
+		assert!(simple_si_units::base::Distance::from(
+			uom::si::f32::Length::new::<uom::si::length::meter>(x)
+		) == simple_si_units::base::Distance{m: x});
+		assert!(simple_si_units::base::Luminosity::from(
+			uom::si::f32::LuminousIntensity::new::<uom::si::luminous_intensity::candela>(x)
+		) == simple_si_units::base::Luminosity{cd: x});
+		assert!(simple_si_units::base::Mass::from(
+			uom::si::f32::Mass::new::<uom::si::mass::kilogram>(x)
+		) == simple_si_units::base::Mass{kg: x});
+		assert!(simple_si_units::base::Temperature::from(
+			uom::si::f32::ThermodynamicTemperature::new::<uom::si::thermodynamic_temperature::kelvin>(x)
+		) == simple_si_units::base::Temperature{K: x});
+		assert!(simple_si_units::base::Time::from(
+			uom::si::f32::Time::new::<uom::si::time::second>(x)
+		) == simple_si_units::base::Time{s: x});
+		assert!(simple_si_units::chemical::CatalyticActivity::from(
+			uom::si::f32::CatalyticActivity::new::<uom::si::catalytic_activity::mole_per_second>(x)
+		) == simple_si_units::chemical::CatalyticActivity{molps: x});
+		assert!(simple_si_units::chemical::Concentration::from(
+			uom::si::f32::MolarConcentration::new::<uom::si::molar_concentration::mole_per_cubic_meter>(x)
+		) == simple_si_units::chemical::Concentration{molpm3: x});
+		assert!(simple_si_units::electromagnetic::Capacitance::from(
+			uom::si::f32::Capacitance::new::<uom::si::capacitance::farad>(x)
+		) == simple_si_units::electromagnetic::Capacitance{F: x});
+		assert!(simple_si_units::electromagnetic::Charge::from(
+			uom::si::f32::ElectricCharge::new::<uom::si::electric_charge::coulomb>(x)
+		) == simple_si_units::electromagnetic::Charge{C: x});
+		assert!(simple_si_units::electromagnetic::Illuminance::from(
+			uom::si::f32::Luminance::new::<uom::si::luminance::candela_per_square_meter>(x)
+		) == simple_si_units::electromagnetic::Illuminance{lux: x});
+		assert!(simple_si_units::electromagnetic::Inductance::from(
+			uom::si::f32::Inductance::new::<uom::si::inductance::henry>(x)
+		) == simple_si_units::electromagnetic::Inductance{H: x});
+		assert!(simple_si_units::electromagnetic::MagneticFlux::from(
+			uom::si::f32::MagneticFlux::new::<uom::si::magnetic_flux::weber>(x)
+		) == simple_si_units::electromagnetic::MagneticFlux{Wb: x});
+		assert!(simple_si_units::electromagnetic::MagneticFluxDensity::from(
+			uom::si::f32::MagneticFluxDensity::new::<uom::si::magnetic_flux_density::tesla>(x)
+		) == simple_si_units::electromagnetic::MagneticFluxDensity{T: x});
+		assert!(simple_si_units::electromagnetic::Resistance::from(
+			uom::si::f32::ElectricalResistance::new::<uom::si::electrical_resistance::ohm>(x)
+		) == simple_si_units::electromagnetic::Resistance{Ohm: x});
+		assert!(simple_si_units::electromagnetic::Voltage::from(
+			uom::si::f32::ElectricPotential::new::<uom::si::electric_potential::volt>(x)
+		) == simple_si_units::electromagnetic::Voltage{V: x});
+		assert!(simple_si_units::geometry::Angle::from(
+			uom::si::f32::Angle::new::<uom::si::angle::radian>(x)
+		) == simple_si_units::geometry::Angle{rad: x});
+		assert!(simple_si_units::geometry::Area::from(
+			uom::si::f32::Area::new::<uom::si::area::square_meter>(x)
+		) == simple_si_units::geometry::Area{m2: x});
+		assert!(simple_si_units::geometry::Volume::from(
+			uom::si::f32::Volume::new::<uom::si::volume::cubic_meter>(x)
+		) == simple_si_units::geometry::Volume{m3: x});
+		assert!(simple_si_units::mechanical::Acceleration::from(
+			uom::si::f32::Acceleration::new::<uom::si::acceleration::meter_per_second_squared>(x)
+		) == simple_si_units::mechanical::Acceleration{mps2: x});
+		assert!(simple_si_units::mechanical::AngularAcceleration::from(
+			uom::si::f32::AngularAcceleration::new::<uom::si::angular_acceleration::radian_per_second_squared>(x)
+		) == simple_si_units::mechanical::AngularAcceleration{radps2: x});
+		assert!(simple_si_units::mechanical::AngularVelocity::from(
+			uom::si::f32::AngularVelocity::new::<uom::si::angular_velocity::radian_per_second>(x)
+		) == simple_si_units::mechanical::AngularVelocity{radps: x});
+		assert!(simple_si_units::mechanical::AreaDensity::from(
+			uom::si::f32::ArealMassDensity::new::<uom::si::areal_mass_density::kilogram_per_square_meter>(x)
+		) == simple_si_units::mechanical::AreaDensity{kgpm2: x});
+		assert!(simple_si_units::mechanical::Density::from(
+			uom::si::f32::MassDensity::new::<uom::si::mass_density::kilogram_per_cubic_meter>(x)
+		) == simple_si_units::mechanical::Density{kgpm3: x});
+		assert!(simple_si_units::mechanical::Energy::from(
+			uom::si::f32::Energy::new::<uom::si::energy::joule>(x)
+		) == simple_si_units::mechanical::Energy{J: x});
+		assert!(simple_si_units::mechanical::Force::from(
+			uom::si::f32::Force::new::<uom::si::force::newton>(x)
+		) == simple_si_units::mechanical::Force{N: x});
+		assert!(simple_si_units::mechanical::Frequency::from(
+			uom::si::f32::Frequency::new::<uom::si::frequency::hertz>(x)
+		) == simple_si_units::mechanical::Frequency{Hz: x});
+		assert!(simple_si_units::mechanical::MomentOfInertia::from(
+			uom::si::f32::MomentOfInertia::new::<uom::si::moment_of_inertia::kilogram_square_meter>(x)
+		) == simple_si_units::mechanical::MomentOfInertia{kgm2: x});
+		assert!(simple_si_units::mechanical::Momentum::from(
+			uom::si::f32::Momentum::new::<uom::si::momentum::kilogram_meter_per_second>(x)
+		) == simple_si_units::mechanical::Momentum{kgmps: x});
+		assert!(simple_si_units::mechanical::Power::from(
+			uom::si::f32::Power::new::<uom::si::power::watt>(x)
+		) == simple_si_units::mechanical::Power{W: x});
+		assert!(simple_si_units::mechanical::Pressure::from(
+			uom::si::f32::Pressure::new::<uom::si::pressure::pascal>(x)
+		) == simple_si_units::mechanical::Pressure{Pa: x});
+		assert!(simple_si_units::mechanical::Torque::from(
+			uom::si::f32::Torque::new::<uom::si::torque::newton_meter>(x)
+		) == simple_si_units::mechanical::Torque{Nm: x});
+		assert!(simple_si_units::mechanical::Velocity::from(
+			uom::si::f32::Velocity::new::<uom::si::velocity::meter_per_second>(x)
+		) == simple_si_units::mechanical::Velocity{mps: x});
+		assert!(simple_si_units::nuclear::Radioactivity::from(
+			uom::si::f32::Radioactivity::new::<uom::si::radioactivity::becquerel>(x)
+		) == simple_si_units::nuclear::Radioactivity{Bq: x});
+	}
+
 }
